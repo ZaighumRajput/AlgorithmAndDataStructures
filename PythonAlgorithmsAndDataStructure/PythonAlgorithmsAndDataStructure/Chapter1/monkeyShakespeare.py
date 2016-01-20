34fea0844bf52e48473405ba8f040c37e3cfe9f8
Zaighum Rajput <zaighum47@gmail.com>
2016-01-19 23:42:53 -0500

    Added Random_String_Generator missing Unit Tests

--- PythonAlgorithmsAndDataStructure/PythonAlgorithmsAndDataStructure/Chapter1/monkeyShakespeare.py
+++ PythonAlgorithmsAndDataStructure/PythonAlgorithmsAndDataStructure/Chapter1/monkeyShakespeare.py
@@ -1,9 +1,18 @@
-﻿def randomStringGenerator(lengthOfString, charactersAllowed):
+﻿import unittest
+from random import choice
+
+def Random_String_Generator(lengthOfString, listOfCharactersAllowed):
     """
     Generates a string of given length using characters from list provided
 
 
     """
-    pass
+    return "".join(choice(listOfCharactersAllowed) for _ in range(lengthOfString))
 
-
+class Tests_Random_String_Generator(unittest.test):
+    
+    def SmokeTest(self):
+        """
+        Check if function exists and generates no errors
+        """
+        Random_String_Generator(5, ['a','b','c'])

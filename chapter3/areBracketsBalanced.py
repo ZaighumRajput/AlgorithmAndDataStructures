e3b6b21ab13746b4b0e3a18971b638616f68da3f
zaighumrajput <zaighum47@gmail.com>
2016-10-23 19:35:22 -0400

    extended for other brackets

--- chapter3/areBracketsBalanced.py
+++ chapter3/areBracketsBalanced.py
@@ -1,16 +1,26 @@
 import unittest
 import stack
 
+
+
+OPENING_BRACKETS = '({['
+CLOSING_BRACKETS = ')}]'
+
+def doBracketsMatch(openBracket, closeBracket):
+    '''what is a better data structure for this?
+    '''
+    return OPENING_BRACKETS.index(openBracket) == CLOSING_BRACKETS.index(closeBracket)
+
 def areBracketsBalanced(strToCheck):
-    openingBrackets = {'('}
+
     BracketStack = stack.Stack()
     for character in strToCheck:
-        if character in openingBrackets:
+        if character in OPENING_BRACKETS:
             BracketStack.push(character)
         else:
             if BracketStack.isEmpty():
                 return False
-            elif character == ')':
+            elif character in CLOSING_BRACKETS and doBracketsMatch(BracketStack.peek(), character):
                 BracketStack.pop()
 
     #strToCheck is balanced if each opening bracket has been
@@ -23,7 +33,9 @@ class test_areBracketsBalanced(unittest.TestCase):
     def test_true(self):
         ListOfBalancedTestStrings = ['(1)',
                             '(1+(1+2))',
-                            '(1+(1+(3 + x)))']
+                            '(1+(1+(3 + x)))',
+                                     '([1])',
+                                     '[(1+1) in {a}]']
 
         for testString in ListOfBalancedTestStrings:
             self.assertTrue(areBracketsBalanced(testString))
@@ -31,7 +43,9 @@ class test_areBracketsBalanced(unittest.TestCase):
     def test_false(self):
         ListOfUnBalancedTestStrings = ['( 1',
                                        '( 1 + (1 + 2)',
-                                       '( 1 + ( + ( )']
+                                       '( 1 + ( + ( )',
+                                       '[{',
+                                       ']']
         for testString in ListOfUnBalancedTestStrings:
             self.assertFalse(areBracketsBalanced(testString))
 
@@ -49,6 +63,15 @@ class test_areBracketsBalanced(unittest.TestCase):
         for testString in ListOfProblematicTestStrings:
             self.assertFalse(areBracketsBalanced(testString))
 
+class test_doBracketsMatch_(unittest.TestCase):
+
+    def test_match(self):
+        self.assertTrue(doBracketsMatch('(', ')'))
+        self.assertTrue(doBracketsMatch('{', '}'))
+        self.assertTrue(doBracketsMatch('[', ']'))
+
+    def test_notMatch(self):
+        self.assertFalse(doBracketsMatch('(', ']'))
 
 if __name__ == '__main__':
     unittest.main()
